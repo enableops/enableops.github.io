ac9283857c9ccb0d0886e69ec40698140d3c9370
Andrei Chenchik <andrei@chenchik.me>
2021-12-19 17:51:07 +0100

    Fix url & CNAME settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'enableOps',
   tagline: 'Throw off the shackles of cloud infrastructure complexity',
-  url: 'https://enableops.github.io',
+  url: 'https://enableops.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
