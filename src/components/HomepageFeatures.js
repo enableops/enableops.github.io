59e9fea2cd89954d369499197f5be2df426a7ddb
Andrei Chenchik <andrei@chenchik.me>
2022-01-20 16:14:26 +0000

    rollback unmodified file

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,43 +1,42 @@
-import React from "react";
-import clsx from "clsx";
-import styles from "./HomepageFeatures.module.css";
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: "Easy to Use",
-    Svg: require("../../static/img/easy.svg").default,
+    title: 'Easy to Use',
+    Svg: require('../../static/img/easy.svg').default,
     description: (
       <>
-        enableOps was designed from the ground up to be easily installed and get
-        your infrastructure up and running quickly.
+        enableOps was designed from the ground up to be easily installed and
+        used to get your infrastructure up and running quickly.
       </>
     ),
   },
   {
-    title: "Focus on What Matters",
-    Svg: require("../../static/img/matters.svg").default,
+    title: 'Focus on What Matters',
+    Svg: require('../../static/img/matters.svg').default,
     description: (
       <>
-        enableOps lets you focus on development, while we&apos;ll do the chores,
-        like infrastructure or security.
+        enableOps lets you focus on your development, and we&apos;ll do the chores, like infrastructure or security.
       </>
     ),
   },
   {
-    title: "Infrastructure as Code",
-    Svg: require("../../static/img/terraform.svg").default,
+    title: 'Powered by Terraform',
+    Svg: require('../../static/img/terraform.svg').default,
     description: (
       <>
-        Create or extend clusters via configs in GitHub. We'll provide you with
-        modules which follow industry best practicies.
+        Extend or customize your network layout by changing variables in modules. enableOps can
+        be easily extended while reusing the same configs.
       </>
     ),
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({Svg, title, description}) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
