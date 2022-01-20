cf71128414c9dcb67d4735381aded2a2353d6a85
Andrei Chenchik <andrei@chenchik.me>
2022-01-20 09:32:29 +0000

    Update resources and animation

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,36 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-import TerminalIcon from "@mui/icons-material/Terminal";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import Grid from "@mui/material/Grid";
 import { motion } from "framer-motion";
 import Link from "@docusaurus/Link";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#58a5f0",
-      contrastText: "#fff",
-    },
-  },
-});
+const duration = 6;
+const timeToSwitch = 0.5;
+const switchSpeed = 0.1;
 
-const Item = styled(Paper)(({ theme }) => ({
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
-const duration = 12;
-const timeToSwitch = 0.2;
-const switchSpeed = 0.05;
-
-const animatedButton = (image, x, y, rotate) => {
+const floatingBlock = (image, height, scale, x, y, rotate) => {
+  const randTimeToSwitch = timeToSwitch * (1 + Math.random() * 0.15);
+  const randSwitchSpeed = switchSpeed * (1 + Math.random() * 0.5);
   return (
     <div
       style={{
@@ -42,24 +24,26 @@ const animatedButton = (image, x, y, rotate) => {
       }}
     >
       <motion.div
-        style={{ scale: 0.0, y: 150 }}
+        style={{ x: x[0], y: y[0], scale: scale[0], rotate: rotate[0] }}
         animate={{
-          x: [0, x, x],
-          y: [150, y, y],
-          scale: [0.0, 0.5, 0.5],
-          rotate: [0, rotate, rotate],
+          x: [x[0], x[1], x[1]],
+          y: [y[0], y[1], y[1]],
+          scale: [scale[0], scale[1], scale[1]],
+          rotate: [rotate[0], rotate[1], rotate[1]],
         }}
         transition={{
-          times: [timeToSwitch, timeToSwitch + switchSpeed, 1],
+          times: [randTimeToSwitch, randTimeToSwitch + randSwitchSpeed, 1],
           duration: duration,
           repeat: Infinity,
           repeatType: "reverse",
         }}
       >
-        <motion.div
+        <motion.img
+          src={image}
+          style={{ height: height }}
           animate={{
-            scale: [1, 0.95],
-            rotate: [0, -1],
+            scale: [1, 1 - 0.05 * (Math.random() * 2 - 1)],
+            rotate: [0, Math.random() * 2 - 1],
           }}
           transition={{
             ease: "easeInOut",
@@ -67,54 +51,7 @@ const animatedButton = (image, x, y, rotate) => {
             repeat: Infinity,
             repeatType: "reverse",
           }}
-        >
-          <img src={image} style={{ height: 60 }} alt="Logo" />
-        </motion.div>
-      </motion.div>
-    </div>
-  );
-};
-
-const animatedBlock = (image, x, y, rotate) => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        width: "100%",
-        height: "100%",
-        top: 0,
-        left: 0,
-      }}
-    >
-      <motion.div
-        style={{ scale: 0.0 }}
-        animate={{
-          x: [0, x, x],
-          y: [0, y, y],
-          scale: [0.0, 0.5, 0.5],
-          rotate: [0, rotate, rotate],
-        }}
-        transition={{
-          times: [timeToSwitch, timeToSwitch + switchSpeed, 1],
-          duration: duration,
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
-      >
-        <motion.div
-          animate={{
-            scale: [1, 0.95],
-            rotate: [0, -1],
-          }}
-          transition={{
-            ease: "easeInOut",
-            duration: 3,
-            repeat: Infinity,
-            repeatType: "reverse",
-          }}
-        >
-          <img src={image} style={{ height: 300 }} alt="Logo" />
-        </motion.div>
+        />
       </motion.div>
     </div>
   );
@@ -122,105 +59,135 @@ const animatedBlock = (image, x, y, rotate) => {
 
 const OverviewFlow = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid
-          container
-          spacing={2}
-          alignItems="flex-end"
-          justifyContent="center"
-        >
-          <Grid item md={5}>
-            <Stack spacing={2} justifyContent="flex-start">
-              <h1 style={{ fontSize: "2.9em" }}>
-                Effortless GitOps, CI/CD on Kubernetes for developers
-              </h1>
-              <Link
-                to="/docs/intro"
-                className="button button--secondary button--lg"
-              >
-                Deploy dev and production in 15 min
-              </Link>
-              <span>
-                🍰 <em>zero knowledge required </em>
-              </span>
-            </Stack>
-          </Grid>
-          <Grid
-            item
-            md={7}
-            style={{ position: "relative", textAlign: "center" }}
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2} alignItems="flex-end" justifyContent="center">
+        <Grid item md={5}>
+          <Stack spacing={2} justifyContent="flex-start">
+            <h1 style={{ fontSize: "2.9em" }}>
+              Effortless GitOps, CI/CD on Kubernetes for developers
+            </h1>
+            <Link
+              to="/docs/intro"
+              className="button button--secondary button--lg"
+            >
+              Deploy dev and production in 15 min
+            </Link>
+            <span>
+              🍰 <em>zero knowledge required </em>
+            </span>
+          </Stack>
+        </Grid>
+        <Grid item md={7} style={{ position: "relative", textAlign: "center" }}>
+          {floatingBlock(
+            "img/banner/vault.png",
+            300,
+            [0.0, 0.6],
+            [80, 120],
+            [30, -90],
+            [0, 22]
+          )}
+          {floatingBlock(
+            "img/banner/tested.png",
+            50,
+            [0.0, 0.72],
+            [80, 189],
+            [148, 78],
+            [0, -18]
+          )}
+          {floatingBlock(
+            "img/banner/gke.png",
+            300,
+            [0.0, 0.51],
+            [80, 195],
+            [30, 35],
+            [0, -15]
+          )}
+          {floatingBlock(
+            "img/banner/terraform.png",
+            300,
+            [0.0, 0.36],
+            [80, 152],
+            [30, 145],
+            [0, 15]
+          )}
+          {floatingBlock(
+            "img/banner/argocd.png",
+            300,
+            [0.0, 0.43],
+            [80, 9],
+            [30, -100],
+            [0, -11]
+          )}
+          {floatingBlock(
+            "img/banner/duplicate.png",
+            50,
+            [0.0, 0.72],
+            [100, -50],
+            [148, 78],
+            [0, 20]
+          )}
+          {floatingBlock(
+            "img/banner/prometheus.png",
+            300,
+            [0.0, 0.26],
+            [80, -37],
+            [30, 0],
+            [0, 20]
+          )}
+          {floatingBlock(
+            "img/banner/rollback.png",
+            50,
+            [0.0, 0.72],
+            [100, -60],
+            [148, 182],
+            [0, -7]
+          )}
+          {floatingBlock(
+            "img/banner/kubernetes.png",
+            300,
+            [0.0, 0.5],
+            [80, 21],
+            [30, 126],
+            [0, 0]
+          )}
+
+          <motion.div
+            style={{ scale: 0.8 }}
+            animate={{
+              scale: [0.8, 0.12],
+              x: [0, 77],
+              y: [0, 11],
+              rotate: [0, 13],
+            }}
+            transition={{
+              times: [timeToSwitch, timeToSwitch + switchSpeed],
+              duration: duration,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
           >
-            {animatedButton("img/hero/tested.png", -120, 210, 0)}
-            {animatedBlock(
-              "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Kubernetes_logo_without_workmark.svg/1234px-Kubernetes_logo_without_workmark.svg.png",
-              0,
-              120,
-              3
-            )}
-            {animatedBlock(
-              "https://blogs.vmware.com/cloudprovider/files/2019/04/og-image-8b3e4f7d-blog-aspect-ratio.png",
-              120,
-              50,
-              -10
-            )}
-            {animatedButton("img/hero/duplicate.png", 200, 40, 0)}
-            {animatedBlock(
-              "https://styles.redditmedia.com/t5_4wmnyu/styles/communityIcon_2pv56jvnhlh71.png",
-              100,
-              -100,
-              12
-            )}
-            {animatedBlock(
-              "https://miro.medium.com/max/566/1*CdKKJPCgmansKROEz_YufA.png",
-              -50,
-              -100,
-              -15
-            )}
-            {animatedButton("img/hero/rollback.png", -180, 30, 0)}
-            {animatedBlock(
-              "https://miro.medium.com/max/918/1*xcQGl-ZqVIOGsfK9QX8fIA.png",
-              -160,
-              0,
-              30
-            )}
             <motion.div
-              style={{ scale: 0.8 }}
               animate={{
-                scale: [0.8, 0.1],
-                x: [0, -20],
-                y: [0, -20],
+                scale: [1, 0.95],
+                rotate: [0, -1],
               }}
               transition={{
-                times: [timeToSwitch, timeToSwitch + switchSpeed],
-                duration: duration,
+                ease: "easeInOut",
+                duration: 3,
                 repeat: Infinity,
                 repeatType: "reverse",
               }}
             >
-              <motion.div
-                animate={{
-                  scale: [1, 0.95],
-                  rotate: [0, -1],
-                }}
-                transition={{
-                  ease: "easeInOut",
-                  duration: 3,
-                  repeat: Infinity,
-                  repeatType: "reverse",
-                }}
-              >
-                <img
-                  src="img/small-logo.png"
-                  style={{ height: 300 }}
-                  alt="Logo"
-                />
-              </motion.div>
+              <img
+                src="img/banner/enableops.svg"
+                style={{ height: 300 }}
+                alt="Logo"
+              />
             </motion.div>
-          </Grid>
+          </motion.div>
         </Grid>
-      </Box>
-    </ThemeProvider>
+      </Grid>
+    </Box>
   );
 };
 
